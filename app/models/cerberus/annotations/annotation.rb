a06631f35b87519d4773c457192af2a7ad2cbe9f
Naomi Dushay <ndushay@stanford.edu>
2014-09-15 17:58:48 -0700

    implement has_body for blank node with text

--- app/models/cerberus/annotations/annotation.rb
+++ app/models/cerberus/annotations/annotation.rb
@@ -6,7 +6,7 @@ module Cerberus::Annotations
     end
 
     def type
-      s = rdf.detect { |s| 
+      rdf.detect { |s| 
         s.predicate.to_s == "http://www.w3.org/1999/02/22-rdf-syntax-ns#type"
       }.object.to_str
     end
@@ -14,10 +14,33 @@ module Cerberus::Annotations
     def has_target
       # FIXME:  can have multiple targets per spec  (example 8)
       # FIXME:  target might be more than a string (examples 14-17)
-      s = rdf.find_all { |s| 
+      stmt = rdf.find_all { |s| 
         s.predicate.to_s == "http://www.w3.org/ns/oa#hasTarget"
       }.first
-      s.object.to_str
+      stmt.object.to_str if stmt
+    end
+    
+    def has_body
+      # FIXME:  can have multiple bodies per spec
+      stmt = rdf.find_all { |s| 
+        s.predicate.to_s == "http://www.w3.org/ns/oa#hasBody"
+      }.first
+      
+      # FIXME:  body can be other things
+      # if body is blank node and has character content, then return it
+      body = stmt.object if stmt
+      if body && body.is_a?(RDF::Node)
+        body_stmts = rdf.find_all { |s| s.subject == body }
+        if body_stmts && 
+            body_stmts.detect { |s| 
+              s.predicate.to_s == "http://www.w3.org/1999/02/22-rdf-syntax-ns#type" &&
+              s.object.to_s == "http://www.w3.org/2011/content#ContentAsText"
+            }
+          chars_stmt = body_stmts.detect { |s| s.predicate.to_s == "http://www.w3.org/2011/content#chars"}
+          return chars_stmt.object.to_s if chars_stmt
+        end
+      end
+      nil
     end
 
     def motivated_by
--- spec/models/cerberus/annotations/annotation_spec.rb
+++ spec/models/cerberus/annotations/annotation_spec.rb
@@ -17,8 +17,17 @@ describe Cerberus::Annotations::Annotation do
     it "url is the @id of the json" do
       expect(@anno.url).to eql("http://example.org/annos/annotation/12.json")
     end
-    it "has_target as URL" do
+    it "has_target when URL" do
       expect(@anno.has_target).to eql("http://purl.stanford.edu/kq131cs7229")
+    end
+    it "has_body missing" do
+      expect(@anno.has_body).to be_nil
+    end
+    it "has_body when blank node with text" do
+      anno = Cerberus::Annotations::Annotation.new data: annotation_fixture("annotation-comment-as-text-chars.json")
+      expect(anno.has_body).to eql("I love this!")
+      anno = Cerberus::Annotations::Annotation.new data: annotation_fixture("annotation-multiple-targets.json")
+      expect(anno.has_body).to eql("I love these two things!")
     end
     it "motivated_by" do
       expect(@anno.motivated_by).to eql("http://www.w3.org/ns/oa#bookmarking")
