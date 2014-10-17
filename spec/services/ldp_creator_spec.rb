3613073696ed9411894d936508feb2d8246f2a86
Naomi Dushay <ndushay@stanford.edu>
2014-10-17 11:50:50 -0700

    refactor LdpCreator.subject specs

--- spec/services/ldp_creator_spec.rb
+++ spec/services/ldp_creator_spec.rb
@@ -124,30 +124,39 @@ describe Triannon::LdpCreator, :vcr => vcr_options do
   
   describe '#subject_statements' do
     it 'returns appropriate blank node statements when the subject is an RDF::Node in the graph' do
-      # we know anno's graph has a body with a blank node
-      has_body_stmts = anno.graph.query([nil, RDF::OpenAnnotation.hasBody, nil])
-      expect(has_body_stmts.size).to eql 1
-      body_resource = has_body_stmts.first.object
-      expect(body_resource).to be_a RDF::Node
-
-      body_stmts = svc.send(:subject_statements, body_resource, anno.graph)
+      graph = RDF::Graph.new
+      graph.from_ttl('<http://example.org/annos/annotation/body-chars.ttl> <http://www.w3.org/ns/oa#hasBody> [
+           a <http://www.w3.org/2011/content#ContentAsText>,
+             <http://purl.org/dc/dcmitype/Text>;
+           <http://www.w3.org/2011/content#chars> "I love this!"
+         ] .')
+      body_resource = graph.query([nil, RDF::OpenAnnotation.hasBody, nil]).first.object
+      body_stmts = svc.send(:subject_statements, body_resource, graph)
       expect(body_stmts.size).to eql 3
       expect(body_stmts).to include([body_resource, RDF::Content::chars, "I love this!"])
       expect(body_stmts).to include([body_resource, RDF.type, RDF::Content.ContentAsText])
       expect(body_stmts).to include([body_resource, RDF.type, RDF::DCMIType.Text])
     end
-    it 'recurses when the object of a subject statement is itself a blank node' do
+    it 'recurses to get triples from objects of the subject statements' do
       graph = RDF::Graph.new
-      graph.from_jsonld(Triannon.annotation_fixture("html-frag-pos-selector.json"))
-      has_target_stmts = graph.query([nil, RDF::OpenAnnotation.hasTarget, nil])
-      target_resource = has_target_stmts.first.object
-
+      graph.from_jsonld('{
+        "@context": "http://www.w3.org/ns/oa-context-20130208.json", 
+        "hasTarget": {
+          "@type": "oa:SpecificResource", 
+          "hasSource": "http://purl.stanford.edu/kq131cs7229.html", 
+          "hasSelector": {
+            "@type": "oa:TextPositionSelector", 
+            "start": 0, 
+            "end": 66
+          }
+        }
+      }')
+      target_resource = graph.query([nil, RDF::OpenAnnotation.hasTarget, nil]).first.object
       target_stmts = svc.send(:subject_statements, target_resource, graph)
       expect(target_stmts.size).to eql 6
       expect(target_stmts).to include([target_resource, RDF.type, RDF::OpenAnnotation.SpecificResource])
       expect(target_stmts).to include([target_resource, RDF::OpenAnnotation.hasSource, "http://purl.stanford.edu/kq131cs7229.html"])
-      has_selector_stmts = graph.query([target_resource, RDF::OpenAnnotation.hasSelector, nil])
-      selector_resource = has_selector_stmts.first.object
+      selector_resource =  graph.query([target_resource, RDF::OpenAnnotation.hasSelector, nil]).first.object
       expect(target_stmts).to include([target_resource, RDF::OpenAnnotation.hasSelector, selector_resource])
       expect(target_stmts).to include([selector_resource, RDF.type, RDF::OpenAnnotation.TextPositionSelector])
       expect(target_stmts).to include([selector_resource, RDF::OpenAnnotation.start, RDF::Literal.new(0)])
@@ -156,16 +165,27 @@ describe Triannon::LdpCreator, :vcr => vcr_options do
     it 'empty Array when the subject is an RDF::Node not in the graph' do
       expect(svc.send(:subject_statements, RDF::Node.new, anno.graph)).to eql []
     end
-    it 'empty Array when the subject is an RDF::URI' do
-      # we know anno's graph has a target with a URI
-      has_target_stmts = anno.graph.query([nil, RDF::OpenAnnotation.hasTarget, nil])
-      expect(has_target_stmts.size).to eql 1
-      target_resource = has_target_stmts.first.object
+    it 'empty Array when the subject is not in the graph' do
+      graph = RDF::Graph.new
+      graph.from_ttl('<http://example.org/annos/annotation/body-chars.ttl> <http://www.w3.org/ns/oa#hasBody> [
+           a <http://www.w3.org/2011/content#ContentAsText>,
+             <http://purl.org/dc/dcmitype/Text>;
+           <http://www.w3.org/2011/content#chars> "I love this!"
+         ] .')
+      expect(svc.send(:subject_statements, RDF::Node.new, graph)).to eql []
+      expect(svc.send(:subject_statements, RDF::URI.new("http://not.there.org"), graph)).to eql []
+    end
+    it 'empty Array when the subject is an RDF::URI with no additional properties' do
+      graph = RDF::Graph.new
+      graph.from_ttl('<http://example.org/annos/annotation/body-chars.ttl> <http://www.w3.org/ns/oa#hasTarget> <http://purl.stanford.edu/kq131cs7229>.')
+      target_resource = graph.query([nil, RDF::OpenAnnotation.hasTarget, nil]).first.object
       expect(target_resource).to be_a RDF::URI
-      expect(svc.send(:subject_statements, target_resource, anno.graph)).to eql []
+      expect(svc.send(:subject_statements, target_resource, graph)).to eql []
     end
     it 'empty Array when subject is not RDF::Node or RDF::URI' do
-      expect(svc.send(:subject_statements, nil, anno.graph)).to eql []
+      graph = RDF::Graph.new
+      graph.from_ttl('<http://example.org/annos/annotation/body-chars.ttl> <http://www.w3.org/ns/oa#hasTarget> <http://purl.stanford.edu/kq131cs7229>.')
+      expect(svc.send(:subject_statements, nil, graph)).to eql []
     end
   end
 
