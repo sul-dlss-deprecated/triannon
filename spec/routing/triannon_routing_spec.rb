310a728e6d7033846bd7b818571f6b172d9e2d2a
Naomi Dushay <ndushay@stanford.edu>
2014-12-02 11:38:21 -0800

    add query params tests to routing spec for jsonld_context

--- spec/routing/triannon_routing_spec.rb
+++ spec/routing/triannon_routing_spec.rb
@@ -12,12 +12,33 @@ describe Triannon::AnnotationsController, type: :routing do
       it 'routes /annotation/oa/:id (GET) to #show with jsonld_context of oa in annotations controller' do
         expect(:get => "/annotations/oa/666").to route_to(:controller => "triannon/annotations", :action => "show", :jsonld_context => "oa", :id => "666")
       end
+      it '/annotation/IIIF/:id (GET) should not be available' do
+        expect(:get => "/annotations/IIIF/666").to_not be_routable
+      end
+      it '/annotation/OA/:id (GET) should not be available' do
+        expect(:get => "/annotations/OA/666").to_not be_routable
+      end
       it '/annotation/foo/:id (GET) should not be available' do
         expect(:get => "/annotations/foo/666").to_not be_routable
       end
       it 'routes /annotation/:id (GET) to #show in annotations controller' do
         expect(:get => "/annotations/666").to route_to(:controller => "triannon/annotations", :action => "show", :id => "666")
-      end    
+      end
+      it 'routes /annotations/666?jsonld_context=iiif to #show with jsonld_context of iiif in annotations controller' do
+        expect(:get => "/annotations/666?jsonld_context=iiif").to route_to(:controller => "triannon/annotations", :action => "show", :jsonld_context => "iiif", :id => "666")
+      end
+      it 'routes /annotations/666?jsonld_context=IIIF to #show with jsonld_context of IIIF in annotations controller' do
+        expect(:get => "/annotations/666?jsonld_context=IIIF").to route_to(:controller => "triannon/annotations", :action => "show", :jsonld_context => "IIIF", :id => "666")
+      end
+      it 'routes /annotations/666?jsonld_context=oa to #show with jsonld_context of oa in annotations controller' do
+        expect(:get => "/annotations/666?jsonld_context=oa").to route_to(:controller => "triannon/annotations", :action => "show", :jsonld_context => "oa", :id => "666")
+      end
+      it 'routes /annotations/666?jsonld_context=OA to #show with jsonld_context of OA in annotations controller' do
+        expect(:get => "/annotations/666?jsonld_context=OA").to route_to(:controller => "triannon/annotations", :action => "show", :jsonld_context => "OA", :id => "666")
+      end
+      it 'routes /annotations/666?jsonld_context=foo to #show with jsonld_context of oa in annotations controller' do
+        expect(:get => "/annotations/666?jsonld_context=foo").to route_to(:controller => "triannon/annotations", :action => "show", :jsonld_context => "foo", :id => "666")
+      end
     end
     
     context 'create action' do
