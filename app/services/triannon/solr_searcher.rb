01fcc9d3a0374a1990e62aee893bf3587ddf2844
Naomi Dushay <ndushay@stanford.edu>
2015-03-23 17:10:49 -0700

    move solr param manipulation methods from search controller to solr_searcher service

--- app/services/triannon/solr_searcher.rb
+++ app/services/triannon/solr_searcher.rb
@@ -14,6 +14,104 @@ module Triannon
       }
       result
     end
+    
+    # @note hardcoded Solr search service expectation in generated search params
+    # @note hardcoded mapping of REST params for /search to Solr params
+    #
+    # Convert action request params to appropriate params
+    #   to be sent to the search service as part of a search request
+    #
+    # request params are given in "Annotation Lists in Triannon" by Robert Sanderson
+    #   in Google Docs:
+    #
+    # - targetUri, value is a URI
+    # - bodyUri, value is a URI
+    # - bodyExact, value is a string
+    # - bodyKeyword, value is a string
+    # - bodyType, value is a URI
+    # - motivatedBy, value is a URI (or just the fragment portion)
+    # - annotatedBy, value is a URI
+    # - annotatedAt, value is a datetime
+    #
+    # @param [Hash<String => String>] controller_params params from Controller
+    # @return [Hash] params to send to Solr as a Hash
+    def self.solr_params(controller_params)
+      solr_params_hash = {}
+      q_terms_array = []
+      fq_terms_array = []
+
+      controller_params.each_pair { |k, v|
+        case k.downcase
+          when 'targeturi'
+            q_terms_array << q_terms_for_url("target_url", v)
+          when 'bodyuri'
+            q_terms_array << q_terms_for_url("body_url", v)
+          when 'bodyexact'
+            # no need to Solr escape value because it's in quotes
+            q_terms_array << "body_chars_exact:\"#{v}\""
+          when 'motivatedby'
+            case
+              when v.include?('#')
+                # we want fragment portion of URL value only, as that
+                # is what is in Solr
+                fq_terms_array << "motivation:#{RSolr.solr_escape(v.sub(/^.*#/, ''))}"
+              when v == "http://www.shared-canvas.org/ns/painting", v == "sc:painting"
+                fq_terms_array << "motivation:painting"
+              else
+                fq_terms_array << "motivation:#{RSolr.solr_escape(v)}"
+            end
+          when 'bodykeyword'
+            solr_params_hash[:kqf] = 'body_chars_exact^3 body_chars_unstem^2 body_chars_stem'
+            solr_params_hash[:kpf] = 'body_chars_exact^15 body_chars_unstem^10 body_chars_stem^5'
+            solr_params_hash[:kpf3] = 'body_chars_exact^9 body_chars_unstem^6 body_chars_stem^3'
+            solr_params_hash[:kpf2] = 'body_chars_exact^6 body_chars_unstem^4 body_chars_stem^2'
+            q_terms_array << '_query_:"{!dismax qf=$kqf pf=$kpf pf3=$kpf3 pf2=$kpf2}' + RSolr.solr_escape(v) + '"'
+
+          # TODO: add'l params to implement:
+          # targetType - fq
+          # bodyType - fq
+          # annotatedAt - fq (deal with time format and wildcard for specificity)
+          # annotatedBy - q (may be incomplete string)
+        end
+      }
+
+      q_terms_array.flatten
+      if q_terms_array.size > 0
+        solr_params_hash[:q] = q_terms_array.join(' AND ')
+        solr_params_hash[:defType] = "lucene"
+      end
+      if fq_terms_array.size > 0
+        solr_params_hash[:fq] = fq_terms_array
+      end
+
+      solr_params_hash
+
+      # TODO:  integration tests for
+      #  target_url with and without the scheme prefix
+      #  target_url with and without fragment
+      #  bodykeyword single terms, multiple terms, quoted strings ...
+
+    end # solr_params
+
+
+    # If the url contains a fragment, query terms should only match the exact
+    #   url given (with the specific fragment).  (i.e. foo.org#bar does not
+    #   match foo.org)
+    # If the url does NOT contain a fragment, query terms should match the
+    #   url given (no fragment) AND any urls that are the same with a fragment
+    #   added.  (i.e. foo.org  matches  foo.org#bar)
+    # @param [String] fieldname the name of the Solr field to be searched with url as a value
+    # @param [String] url the url value sought in the Solr field
+    # @return [Array<String>] an array of query terms to be added to the Solr q argument
+    def self.q_terms_for_url(fieldname, url)
+      q_terms = []
+      q_terms << "#{fieldname}:#{RSolr.solr_escape(url)}"
+      if !url.include? '#'
+        # Note: do NOT Solr escape the # (unnec) or the * (want Solr to view it as wildcard)
+        q_terms << "#{fieldname}:#{RSolr.solr_escape(url)}#*"
+      end
+      q_terms
+    end
 
 
     def initialize
--- spec/services/solr_searcher_spec.rb
+++ spec/services/solr_searcher_spec.rb
@@ -93,4 +93,294 @@ describe Triannon::SolrSearcher, :vcr do
     end
   end
   
+  
+  context '.solr_params' do
+    it "no q param if nothing generates a q term" do
+      expect(Triannon::SolrSearcher.solr_params({})).not_to include :q
+      expect(Triannon::SolrSearcher.solr_params('motivatedBy' => 'commenting')).not_to include :q
+    end
+    it "no fq param if nothing generates fq" do
+      expect(Triannon::SolrSearcher.solr_params({})).not_to include :fq
+      expect(Triannon::SolrSearcher.solr_params('targetUri' => "some.url.org")).not_to include :fq
+    end
+    it "single q param for multiple terms" do
+      solr_params = Triannon::SolrSearcher.solr_params('targetUri' => "some.url.org", 'bodyExact' => "foo")
+      expect(solr_params[:q]).to be_truthy
+      expect(q_includes_term('target_url:some.url.org', solr_params)).to be true
+      expect(q_includes_term('body_chars_exact:"foo"', solr_params)).to be true
+    end
+    it "fq param value is an Array" do
+      solr_params = Triannon::SolrSearcher.solr_params('motivatedBy' => 'commenting')
+      expect(solr_params[:fq]).to be_an Array
+    end
+=begin TODO: implement these tests when we are further along
+    it "multiple fq params" do
+      fail "test to be implemented"
+    end
+=end
+
+    context 'targetUri' do
+      it "creates term in q param" do
+        expect(Triannon::SolrSearcher.solr_params('targetUri' => "some.url.org")).to include :q
+      end
+      it "causes Solr defType=lucene" do
+        expect(Triannon::SolrSearcher.solr_params('targetUri' => "some.url.org")).to include :defType => 'lucene'
+      end
+      it "maps to Solr target_url" do
+        expect(q_includes_term('target_url:some.url.org', Triannon::SolrSearcher.solr_params('targetUri' => "some.url.org"))).to be true
+      end
+      it "calls q_terms_for_url" do
+        expect(Triannon::SolrSearcher).to receive(:q_terms_for_url).with("target_url", "some.url.org")
+        Triannon::SolrSearcher.solr_params('targetUri' => "some.url.org")
+      end
+      it "Solr escapes value" do
+        expect(RSolr).to receive(:solr_escape).at_least(:once).with("some.url.org/foo").and_call_original
+        expect(q_includes_term('target_url:some.url.org\/foo', Triannon::SolrSearcher.solr_params('targetUri' => "some.url.org/foo"))).to be true
+      end
+      it "controller param key not case sensitive" do
+        ['targetUri', 'targeturi', 'TargetUri'].each { |str|
+          expect(q_includes_term('target_url:some.url.org', Triannon::SolrSearcher.solr_params(str => "some.url.org"))).to be true
+        }
+      end
+    end # targetUri
+
+=begin TODO: implement more searching params
+    context 'targetType' do
+      it "creates fq param" do
+        fail "test to be implemented"
+      end
+      it "maps to Solr target_type" do
+        skip "code and tests to be implemented"
+        fail "test to be implemented"
+      end
+      it "works for full url and frag only" do
+        fail "test to be implemented"
+      end
+      it "Solr escapes value" do
+        fail "test to be implemented"
+      end
+    end
+=end
+
+    context 'bodyUri' do
+      it "creates term in q param" do
+        expect(Triannon::SolrSearcher.solr_params('bodyUri' => "some.url.org")).to include :q
+      end
+      it "causes Solr defType=lucene" do
+        expect(Triannon::SolrSearcher.solr_params('bodyUri' => "some.url.org")).to include :defType => 'lucene'
+      end
+      it "maps to Solr body_url" do
+        expect(q_includes_term('body_url:some.url.org', Triannon::SolrSearcher.solr_params('bodyUri' => "some.url.org"))).to be true
+      end
+      it "calls q_terms_for_url" do
+        expect(Triannon::SolrSearcher).to receive(:q_terms_for_url).with("body_url", "some.url.org")
+        Triannon::SolrSearcher.solr_params('bodyUri' => "some.url.org")
+      end
+      it "Solr escapes body_url value" do
+        expect(RSolr).to receive(:solr_escape).at_least(:once).and_call_original
+        expect(q_includes_term('body_url:http\:\/\/some.url.org\/foo', Triannon::SolrSearcher.solr_params('bodyUri' => "http://some.url.org/foo"))).to be true
+      end
+      it "param key not case sensitive" do
+        ['bodyUri', 'bodyuri', 'BodyUri'].each { |str|
+          expect(q_includes_term('body_url:some.url.org', Triannon::SolrSearcher.solr_params(str => "some.url.org"))).to be true
+        }
+      end
+    end # bodyUri
+
+    context 'bodyExact' do
+      it "causes Solr defType=lucene" do
+        expect(Triannon::SolrSearcher.solr_params('bodyExact' => "I'm a tag!")).to include :defType => 'lucene'
+      end
+      it "maps to q term body_chars_exact" do
+        expect(Triannon::SolrSearcher.solr_params('bodyExact' => "tag")).to include :q => "body_chars_exact:\"tag\""
+      end
+      it "adds quotes to value" do
+        expect(Triannon::SolrSearcher.solr_params('bodyExact' => "I'm a tag")).to include :q => "body_chars_exact:\"I'm a tag\""
+      end
+      it "does NOT Solr escape value (because it's in quotes)" do
+        expect(RSolr).not_to receive(:solr_escape)
+        expect(Triannon::SolrSearcher.solr_params('bodyExact' => "I'm a tag! & I [hate|love]")).to include :q => "body_chars_exact:\"I'm a tag! & I [hate|love]\""
+      end
+      it "param key not case sensitive" do
+        ['bodyExact', 'bodyexact', 'BodyExact'].each { |str|
+          expect(Triannon::SolrSearcher.solr_params({str => "tagit"})).to include :q => 'body_chars_exact:"tagit"'
+        }
+      end
+    end # bodyExact
+
+    context 'bodyKeyword' do
+      it "causes Solr defType=lucene" do
+        expect(Triannon::SolrSearcher.solr_params('bodyKeyword' => "word")).to include :defType => 'lucene'
+      end
+      it "kqf param with Solr fields and boosts for deferencing multifield qf" do
+        solr_params = Triannon::SolrSearcher.solr_params('bodyKeyword' => "word")
+        expect(solr_params).to include :kqf => 'body_chars_exact^3 body_chars_unstem^2 body_chars_stem'
+        expect(solr_params).to include :q => a_string_matching(/ qf=\$kqf /)
+      end
+      it "kpf param with Solr fields and boosts for deferencing multifield pf" do
+        solr_params = Triannon::SolrSearcher.solr_params('bodyKeyword' => "word")
+        expect(solr_params).to include :kpf => 'body_chars_exact^15 body_chars_unstem^10 body_chars_stem^5'
+        expect(solr_params).to include :q => a_string_matching(/ pf=\$kpf /)
+      end
+      it "kpf3 param with Solr fields and boosts for deferencing multifield pf3" do
+        solr_params = Triannon::SolrSearcher.solr_params('bodyKeyword' => "word")
+        expect(solr_params).to include :kpf3 => 'body_chars_exact^9 body_chars_unstem^6 body_chars_stem^3'
+        expect(solr_params).to include :q => a_string_matching(/ pf3=\$kpf3 /)
+      end
+      it "kpf2 param with Solr fields and boosts for deferencing multifield pf2" do
+        solr_params = Triannon::SolrSearcher.solr_params('bodyKeyword' => "word")
+        expect(solr_params).to include :kpf2 => 'body_chars_exact^6 body_chars_unstem^4 body_chars_stem^2'
+        expect(solr_params).to include :q => a_string_matching(/ pf2=\$kpf2/)
+      end
+      it "maps to q _query_ term using kqf, kpf, kpf3, kpf2" do
+        expect(Triannon::SolrSearcher.solr_params('bodyKeyword' => "word but")).to include :q => '_query_:"{!dismax qf=$kqf pf=$kpf pf3=$kpf3 pf2=$kpf2}word but"'
+      end
+      it "Solr escapes value" do
+        raw_value = 'solr escape " double quote'
+        expect(RSolr).to receive(:solr_escape).with(raw_value).and_call_original
+        solr_params = Triannon::SolrSearcher.solr_params('bodyKeyword' => raw_value)
+        expect(solr_params).not_to include :q => a_string_matching(raw_value)
+        expect(solr_params).to include :q => a_string_matching(/solr escape \\" double quote/)
+      end
+      it "param key not case sensitive" do
+        ['bodyKeyword', 'bodykeyword', 'BodyKeyword'].each { |str|
+          expect(Triannon::SolrSearcher.solr_params(str => "word")).to include :q => a_string_matching(/word/)
+        }
+      end
+    end
+
+=begin TODO: implement more searching params
+    context 'bodyType' do
+      it "creates fq param" do
+        fail "test to be implemented"
+      end
+      it "maps to Solr body_type" do
+        skip "code and tests to be implemented"
+        fail "test to be implemented"
+      end
+      it "Solr escapes value" do
+        fail "test to be implemented"
+      end
+    end
+=end
+
+    context 'motivatedBy' do
+      it "maps to Solr fq motivation" do
+        expect(Triannon::SolrSearcher.solr_params("motivatedBy" => 'commenting')).to include :fq => ["motivation:commenting"]
+      end
+      it "param value as full URI maps to fragment portion as value" do
+        expect(Triannon::SolrSearcher.solr_params("motivatedBy" => 'http://www.w3.org/ns/oa#commenting')).to include :fq => ["motivation:commenting"]
+      end
+      it "param value can be fragment only" do
+        expect(Triannon::SolrSearcher.solr_params("motivatedBy" => 'commenting')).to include :fq => ["motivation:commenting"]
+      end
+      context 'sc:painting' do
+        it "http://iiif.io/api/presentation/2#painting" do
+          expect(Triannon::SolrSearcher.solr_params('motivatedBy' => 'http://iiif.io/api/presentation/2#painting')).to include :fq => ["motivation:painting"]
+        end
+        it "http://www.shared-canvas.org/ns/painting" do
+          expect(Triannon::SolrSearcher.solr_params('motivatedBy' => 'http://www.shared-canvas.org/ns/painting')).to include :fq => ["motivation:painting"]
+        end
+        it "sc:painting" do
+          expect(Triannon::SolrSearcher.solr_params('motivatedBy' => 'sc:painting')).to include :fq => ["motivation:painting"]
+        end
+        it "painting" do
+          expect(Triannon::SolrSearcher.solr_params('motivatedBy' => 'painting')).to include :fq => ["motivation:painting"]
+        end
+      end
+      it "Solr escapes value" do
+        raw_value = "a!b[c|d]"
+        expect(RSolr).to receive(:solr_escape).with(raw_value).and_call_original
+        solr_params = Triannon::SolrSearcher.solr_params('motivatedBy' => raw_value)
+        expect(solr_params).not_to include :fq => [ (a_string_ending_with(raw_value))]
+        expect(solr_params).to include :fq => [ a_string_ending_with("a\\!b\\[c\\|d\\]") ]
+      end
+      it "param key not case sensitive" do
+        ['motivatedBy', 'motivatedby', 'MotivatedBy'].each { |str|
+          expect(Triannon::SolrSearcher.solr_params(str => "tagging")).to include :fq => ['motivation:tagging']
+        }
+      end
+    end
+
+=begin TODO: implement more searching params
+    context 'annotatedAt' do
+      it "creates fq param" do
+        fail "test to be implemented"
+      end
+      it "maps to Solr annotated_at" do
+        fail "test to be implemented"
+      end
+      it "value is in 1995-12-31T23:59:59Z format " do
+        fail "test to be implemented"
+      end
+      it "value is wildcarded if less specific" do
+        fail "test to be implemented"
+      end
+      it "value is mapped to facet.date.start" do
+        fail "test to be implemented"
+      end
+      it "Solr escapes value" do
+        fail "test to be implemented"
+      end
+    end
+
+    context 'annotatedBy' do
+      it "creates terms in q param" do
+        fail "test to be implemented"
+      end
+      it "maps to Solr annotate_by_unstem" do
+        skip "code and tests to be implemented"
+        fail "test to be implemented"
+      end
+      it "maps to Solr annotate_by_stem" do
+        skip "code and tests to be implemented"
+        fail "test to be implemented"
+      end
+      it "Solr escapes value" do
+        fail "test to be implemented"
+      end
+    end
+=end
+  end # solr_params
+  
+  
+  context '.q_terms_for_url' do
+    let (:fldname) { 'url_solr_field' }
+    let (:url) { 'http://myplace.org' }
+    it "exact match term" do
+      expect(Triannon::SolrSearcher.q_terms_for_url(fldname, url)).to include "#{fldname}:#{RSolr.solr_escape(url)}"
+      expect(Triannon::SolrSearcher.q_terms_for_url(fldname, "#{url}#foo")).to include "#{fldname}:#{RSolr.solr_escape(url + '#foo')}"
+      expect(Triannon::SolrSearcher.q_terms_for_url(fldname, "#{url}/foo")).to include "#{fldname}:#{RSolr.solr_escape(url + '/foo')}"
+      expect(Triannon::SolrSearcher.q_terms_for_url(fldname, "#{url}/foo#bar")).to include "#{fldname}:#{RSolr.solr_escape(url + '/foo#bar')}"
+    end
+    it "frag wildcard term if no frag in url" do
+      expect(Triannon::SolrSearcher.q_terms_for_url(fldname, url)).to include "#{fldname}:#{RSolr.solr_escape(url)}#*"
+      expect(Triannon::SolrSearcher.q_terms_for_url(fldname, "#{url}/foo")).to include "#{fldname}:#{RSolr.solr_escape(url + '/foo')}#*"
+    end
+    it "NO frag wildcard term if frag in url" do
+      expect(Triannon::SolrSearcher.q_terms_for_url(fldname, "#{url}#foo")).not_to include "#{fldname}:#{RSolr.solr_escape(url)}#*"
+      expect(Triannon::SolrSearcher.q_terms_for_url(fldname, "#{url}#foo")).not_to include "#{fldname}:#{RSolr.solr_escape(url + '#foo')}#*"
+      expect(Triannon::SolrSearcher.q_terms_for_url(fldname, "#{url}/foo#bar")).not_to include "#{fldname}:#{RSolr.solr_escape(url + '/foo')}#*"
+      expect(Triannon::SolrSearcher.q_terms_for_url(fldname, "#{url}/foo#bar")).not_to include "#{fldname}:#{RSolr.solr_escape(url + '/foo#bar')}#*"
+      expect(Triannon::SolrSearcher.q_terms_for_url(fldname, "#{url}/foo#bar")).not_to include "#{fldname}:#{RSolr.solr_escape(url)}#*"
+    end
+    it "NO url without frag if frag in url" do
+      expect(Triannon::SolrSearcher.q_terms_for_url(fldname, "#{url}#foo")).not_to include "#{fldname}:#{RSolr.solr_escape(url)}#*"
+    end
+    it "calls RSolr.escape on value" do
+      expect(RSolr).to receive(:solr_escape).with(url).exactly(2).times
+      Triannon::SolrSearcher.q_terms_for_url(fldname, url)
+    end
+  end #q_terms_for_url
+  
+end
+
+# handy for testing when multiple q terms are created
+def q_includes_term term, solr_params
+  qterms = solr_params[:q].split
+  if qterms.include? term
+    true
+  else
+    false
+  end
 end
