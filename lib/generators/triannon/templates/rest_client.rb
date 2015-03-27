c31ca953ae25c9de8b2aa5502f6a496782bdb1dc
Naomi Dushay <ndushay@stanford.edu>
2015-03-27 14:10:40 -0700

    configure Rack::Cache to recheck freshness daily and set verbose to false

--- lib/generators/triannon/templates/rest_client.rb
+++ lib/generators/triannon/templates/rest_client.rb
@@ -2,4 +2,6 @@ require 'restclient/components'
 require 'rack/cache'
 RestClient.enable Rack::Cache,
   metastore: "file:#{Rails.root}/tmp/rack-cache/meta",
-  entitystore: "file:#{Rails.root}/tmp/rack-cache/body"
+  entitystore: "file:#{Rails.root}/tmp/rack-cache/body",
+  default_ttl:  86400, # when to recheck, in seconds (daily = 60 x 60 x 24)
+  verbose: false
