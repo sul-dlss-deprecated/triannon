5a9bebc5bebede940031e95eb2a9aa1fc8fc8e07
Naomi Dushay <ndushay@stanford.edu>
2015-05-19 15:52:49 -0700

    search routing works with anno_root in path

--- config/routes.rb
+++ config/routes.rb
@@ -1,75 +1,81 @@
 Triannon::Engine.routes.draw do
 
   # 1. can't use resourceful routing because of :anno_root (dynamic path segment)
+
   # 2. couldn't figure out how to exclude specific values with regexp constraint since beginning and end regex matchers
   #    aren't allowed when enforcing formats for path segment (i.e. :anno_root, :id)
 
+  # get -> new action
   get '/annotations/:anno_root/new', to: 'annotations#new'
   get '/:anno_root/new', to: 'annotations#new',
     constraints: lambda { |request|
-                            anno_root = request.path_parameters[:anno_root]
-                            id = request.path_parameters[:id]
-                            anno_root !~ /^annotations$/ && anno_root !~ /^search$/ && anno_root !~ /^new$/ && id !~ /^search$/ && id !~ /^new$/
-                 }
+      anno_root = request.path_parameters[:anno_root]
+      id = request.path_parameters[:id]
+      anno_root !~ /^annotations$/ && anno_root !~ /^search$/ && anno_root !~ /^new$/ && id !~ /^search$/ && id !~ /^new$/
+   	}
 
-  get '/annotations/:anno_root/:id(.:format)', to: 'annotations#show'
-  get '/:anno_root/:id(.:format)', to: 'annotations#show',
-    constraints: lambda { |request|
-                            anno_root = request.path_parameters[:anno_root]
-                            id = request.path_parameters[:id]
-                            anno_root !~ /^annotations$/ && anno_root !~ /^search$/ && anno_root !~ /^new$/ && id !~ /^search$/ && id !~ /^new$/
-                 }
-
-  get '/annotations/:anno_root', to: 'annotations#index',
-    constraints: lambda { |request|
-                            anno_root = request.path_parameters[:anno_root]
-                            anno_root !~ /^search$/ && anno_root !~ /^new$/
-                 }
-  get '/:anno_root', to: 'annotations#index',
-    constraints: lambda { |request|
-                            anno_root = request.path_parameters[:anno_root]
-                            anno_root !~ /^annotations$/ && anno_root !~ /^search$/ && anno_root !~ /^new$/
-                 }
-
-  post '/annotations/:anno_root', to: 'annotations#create'
-  post '/:anno_root', to: 'annotations#create',
-    constraints: lambda { |request|
-                            anno_root = request.path_parameters[:anno_root]
-                            anno_root !~ /^annotations$/ && anno_root !~ /^search$/ && anno_root !~ /^new$/
-                 }
-
-  delete '/annotations/:anno_root/:id(.:format)', to: 'annotations#destroy'
-  delete '/:anno_root/:id(.:format)', to: 'annotations#destroy',
-    constraints: lambda { |request|
-                            anno_root = request.path_parameters[:anno_root]
-                            id = request.path_parameters[:id]
-                            anno_root !~ /^annotations$/ && anno_root !~ /^search$/ && anno_root !~ /^new$/ && id !~ /^search$/ && id !~ /^new$/
-                 }
-
+  # get -> search controller find action
   get '/annotations/:anno_root/search', to: 'search#find'
+  get '/annotations/search', to: 'search#find'
   get '/:anno_root/search', to: 'search#find',
     constraints: lambda { |request|
-                            anno_root = request.path_parameters[:anno_root]
-                            anno_root !~ /^annotations$/ && anno_root !~ /^search$/ && anno_root !~ /^new$/
-                 }
+      anno_root = request.path_parameters[:anno_root]
+      anno_root !~ /^annotations$/ && anno_root !~ /^search$/ && anno_root !~ /^new$/
+    }
   get '/search', to: 'search#find'
 
-  root to: 'search#find'
-
-=begin
-  resources :annotations, except: [:update, :edit],
-    # show action must explicitly forbid "search" as id value;  couldn't
-    #   figure out how to do it with regexp constraint since beginning and end regex
-    #   matchers aren't allowed when enforcing formats for segment (e.g. :id)
+  # get w id -> show action
+  get '/annotations/:anno_root/:id(.:format)', to: 'annotations#show',
     constraints: lambda { |request|
-                            id = request.env["action_dispatch.request.path_parameters"][:id]
-                            id !~ /^search$/
-                 } do
-    collection do
-      get 'search', to: 'search#find'
-    end
-  end
-=end
+      anno_root = request.path_parameters[:anno_root]
+      anno_root !~ /^search$/ && anno_root !~ /^new$/
+    }
+  get '/:anno_root/:id(.:format)', to: 'annotations#show',
+    constraints: lambda { |request|
+      anno_root = request.path_parameters[:anno_root]
+      id = request.path_parameters[:id]
+      anno_root !~ /^annotations$/ && anno_root !~ /^search$/ && anno_root !~ /^new$/ && id !~ /^search$/ && id !~ /^new$/
+   	}
 
+  # get no id -> index action
+  get '/annotations/:anno_root', to: 'annotations#index',
+    constraints: lambda { |request|
+      anno_root = request.path_parameters[:anno_root]
+      anno_root !~ /^search$/ && anno_root !~ /^new$/
+    }
+  get '/:anno_root', to: 'annotations#index',
+    constraints: lambda { |request|
+      anno_root = request.path_parameters[:anno_root]
+      anno_root !~ /^annotations$/ && anno_root !~ /^search$/ && anno_root !~ /^new$/
+    }
+
+  # post -> create action
+  post '/annotations/:anno_root', to: 'annotations#create',
+    constraints: lambda { |request|
+      anno_root = request.path_parameters[:anno_root]
+      id = request.path_parameters[:id]
+      anno_root !~ /^search$/ && anno_root !~ /^new$/ && id !~ /^search$/ && id !~ /^new$/
+    }
+  post '/:anno_root', to: 'annotations#create',
+    constraints: lambda { |request|
+      anno_root = request.path_parameters[:anno_root]
+      anno_root !~ /^annotations$/ && anno_root !~ /^search$/ && anno_root !~ /^new$/
+    }
+
+  # delete -> destroy action
+  delete '/annotations/:anno_root/:id(.:format)', to: 'annotations#destroy',
+    constraints: lambda { |request|
+      anno_root = request.path_parameters[:anno_root]
+      id = request.path_parameters[:id]
+      anno_root !~ /^search$/ && anno_root !~ /^new$/ && id !~ /^search$/ && id !~ /^new$/
+    }
+  delete '/:anno_root/:id(.:format)', to: 'annotations#destroy',
+    constraints: lambda { |request|
+      anno_root = request.path_parameters[:anno_root]
+      anno_root !~ /^annotations$/ && anno_root !~ /^search$/ && anno_root !~ /^new$/
+    }
+
+
+  root to: 'search#find'
 
 end
--- spec/routing/triannon_routing_spec.rb
+++ spec/routing/triannon_routing_spec.rb
@@ -155,21 +155,22 @@ describe Triannon::AnnotationsController, type: :routing do
     end
   end
 
-=begin
   context '/search' do
-    it '/annotations routed to triannon/search#find without anno_root param' do
-      expect(:get => "/").to route_to(controller: "triannon/search", action: "find", anno_root: nil)
-    end
-    it "/annotations/:anno_root/search?params (GET) routed to triannon/search#find with params" do
-      expect(:get => "/annotations/dms/search?target=neato.url.org").to route_to(controller: "triannon/search", action: "find", anno_root: "dms", target: "neato.url.org")
-      expect(:get => "/annotations/dms/search?foo=bar").to route_to(controller: "triannon/search", action: "find", anno_root: "dms", :foo => "bar")
+    it "/annotations/:anno_root/search routed to triannon/search#find with anno_root param" do
       expect(:get => "/annotations/dms/search").to route_to(controller: "triannon/search", action: "find", anno_root: "dms")
     end
-    it "/annotations/search?params (GET) routed to triannon/search#find without anno_root param" do
+    it "/annotations/:anno_root/search?params routed to triannon/search#find with anno_root param" do
+      expect(:get => "/annotations/dms/search?target=neato.url.org").to route_to(controller: "triannon/search", action: "find", anno_root: "dms", target: "neato.url.org")
+      expect(:get => "/annotations/dms/search?foo=bar").to route_to(controller: "triannon/search", action: "find", anno_root: "dms", :foo => "bar")
+    end
+    it '/annotations routed to triannon/search#find without anno_root param' do
+      expect(:get => "/").to route_to(controller: "triannon/search", action: "find")
+    end
+    it "/annotations/search?params routed to triannon/search#find without anno_root param" do
       expect(:get => "/annotations/search?target=neato.url.org").to route_to(controller: "triannon/search", action: "find", target: "neato.url.org")
       expect(:get => "/annotations/search?foo=bar").to route_to(controller: "triannon/search", action: "find", :foo => "bar")
     end
-    it "/search?params (GET) routed to triannon/search#find without anno_root param" do
+    it "/search?params routed to triannon/search#find without anno_root param" do
       expect(:get => "/search?target=neato.url.org").to route_to(controller: "triannon/search", action: "find", target: "neato.url.org")
       expect(:get => "/search?foo=bar").to route_to(controller: "triannon/search", action: "find", :foo => "bar")
     end
@@ -201,181 +202,6 @@ describe Triannon::AnnotationsController, type: :routing do
       expect(:patch => "/search").not_to be_routable
       expect(:patch => "/annotations/search/666").not_to be_routable
     end
-  end
-
-=end
-
-# ---   before multiple root anno containers ---
-
-=begin
-
-  context 'http GET with id' do
-    it '/annotation/:id (GET) routes to #show in annotations controller' do
-      expect(:get => "/annotations/666").to route_to(controller: "triannon/annotations", action: "show", id: "666")
-    end
-
-    context 'jsonld_context' do
-      it '/annotation/iiif/:id (GET) routes to #show with jsonld_context of iiif' do
-        expect(:get => "/annotations/iiif/666").to route_to(controller: "triannon/annotations", action: "show", :jsonld_context => "iiif", id: "666")
-      end
-      it '/annotation/oa/:id (GET) routes to #show with jsonld_context of oa' do
-        expect(:get => "/annotations/oa/666").to route_to(controller: "triannon/annotations", action: "show", :jsonld_context => "oa", id: "666")
-      end
-      it '/annotation/IIIF/:id (GET) should not be available' do
-        expect(:get => "/annotations/IIIF/666").not_to be_routable
-      end
-      it '/annotation/OA/:id (GET) should not be available' do
-        expect(:get => "/annotations/OA/666").not_to be_routable
-      end
-      it '/annotation/search/:id (GET) should not be available' do
-        expect(:get => "/annotations/search/666").not_to be_routable
-      end
-      it '/annotation/foo/:id (GET) should not be available' do
-        expect(:get => "/annotations/foo/666").not_to be_routable
-      end
-      it '/annotations/666?jsonld_context=iiif routes to #show with jsonld_context of iiif' do
-        expect(:get => "/annotations/666?jsonld_context=iiif").to route_to(controller: "triannon/annotations", action: "show", :jsonld_context => "iiif", id: "666")
-      end
-      it '/annotations/666?jsonld_context=IIIF routes to #show with jsonld_context of IIIF' do
-        expect(:get => "/annotations/666?jsonld_context=IIIF").to route_to(controller: "triannon/annotations", action: "show", :jsonld_context => "IIIF", id: "666")
-      end
-      it '/annotations/666?jsonld_context=oa routes to #show with jsonld_context of oa' do
-        expect(:get => "/annotations/666?jsonld_context=oa").to route_to(controller: "triannon/annotations", action: "show", :jsonld_context => "oa", id: "666")
-      end
-      it '/annotations/666?jsonld_context=OA routes to #show with jsonld_context of OA' do
-        expect(:get => "/annotations/666?jsonld_context=OA").to route_to(controller: "triannon/annotations", action: "show", :jsonld_context => "OA", id: "666")
-      end
-      it '/annotations/666?jsonld_context=foo routes to #show with jsonld_context of oa' do
-        expect(:get => "/annotations/666?jsonld_context=foo").to route_to(controller: "triannon/annotations", action: "show", :jsonld_context => "foo", id: "666")
-      end
-    end
-  end
-
-  context 'http GET without id' do
-    it '(plain) routes to #index in annotations controller' do
-      expect(:get => "/annotations").to route_to(controller: "triannon/annotations", action: "index")
-    end
-    it 'iiif should not be available' do
-      expect(:get => "/annotations/iiif").not_to be_routable
-    end
-    it 'oa should not be available' do
-      expect(:get => "/annotations/oa").not_to be_routable
-    end
-  end
-
-  context 'http GET to root without id' do
-    it '(plain) routes to #find in search controller' do
-      expect(:get => "/").to route_to(controller: "triannon/search", action: "find")
-    end
-    it 'iiif should not be available' do
-      expect(:get => "/iiif").not_to be_routable
-    end
-    it 'oa should not be available' do
-      expect(:get => "/oa").not_to be_routable
-    end
-  end
-
-  context 'http GET with new' do
-    it '(plain) routes to #new in annotations controller' do
-      expect(:get => "/annotations/new").to route_to(controller: "triannon/annotations", action: "new")
-    end
-    it 'iiif should not be available' do
-      expect(:get => "/annotations/iiif/new").not_to be_routable
-    end
-    it 'oa should not be available' do
-      expect(:get => "/annotations/oa/new").not_to be_routable
-    end
-    it 'search should not be available' do
-      expect(:get => "/annotations/search/new").not_to be_routable
-    end
-  end
-
-  context 'http POST' do
-    it '(plain) routes to #create in annotations controller' do
-      expect(:post => "/annotations").to route_to(controller: "triannon/annotations", action: "create")
-    end
-    it 'iiif should not be available' do
-      expect(:post => "/annotations/iiif").not_to be_routable
-    end
-    it 'oa should not be available' do
-      expect(:post => "/annotations/oa").not_to be_routable
-    end
-  end
-
-  context 'http DELETE with id' do
-    it '(plain) routed to #destroy in annotations controller' do
-      expect(:delete => "/annotations/666").to route_to(controller: "triannon/annotations", action: "destroy", id: "666")
-    end
-    it 'iiif should not be available' do
-      expect(:delete => "/annotations/iiif/666").not_to be_routable
-    end
-    it 'oa should not be available' do
-      expect(:delete => "/annotations/oa/666").not_to be_routable
-    end
-  end
-
-  context 'http PUT' do
-    it '(plain) should not be available' do
-      expect(:put => "/annotations/666").not_to be_routable
-    end
-#    it '(plain) routes to #update in annotations controller' do
-#      expect(:put => "/annotations/666").to route_to(controller: "triannon/annotations", action: "update", id: "666")
-#    end
-    it 'iiif should not be available' do
-      expect(:put => "/annotations/iiif/666").not_to be_routable
-    end
-    it 'oa should not be available' do
-      expect(:put => "/annotations/oa/666").not_to be_routable
-    end
-  end
-
-  context 'http PATCH' do
-    it '(plain) should not be available' do
-      expect(:patch => "/annotations/666").not_to be_routable
-    end
-    it 'iiif should not be available' do
-      expect(:patch => "/annotations/iiif/666").not_to be_routable
-    end
-    it 'oa should not be available' do
-      expect(:patch => "/annotations/oa/666").not_to be_routable
-    end
-  end
-
-  context '/search' do
-    it "/annotations/search?params (GET) routes to #find in search controller with params" do
-      expect(:get => "/annotations/search?target=neato.url.org").to route_to(controller: "triannon/search", action: "find", target: "neato.url.org")
-      expect(:get => "/annotations/search?foo=bar").to route_to(controller: "triannon/search", action: "find", :foo => "bar")
-      expect(:get => "/annotations/search").to route_to(controller: "triannon/search", action: "find")
-    end
-    it "/search?params (GET) routes to #find in search controller with params" do
-      expect(:get => "/search?target=neato.url.org").to route_to(controller: "triannon/search", action: "find", target: "neato.url.org")
-      expect(:get => "/search?foo=bar").to route_to(controller: "triannon/search", action: "find", :foo => "bar")
-    end
-    it "does not take an id" do
-      expect(:get => "/annotations/search/666").not_to be_routable
-      expect(:get => "/search/666").not_to be_routable
-    end
-    it "http POST should not be available" do
-      expect(:post => "/annotations/search").not_to be_routable
-      expect(:post => "/search").not_to be_routable
-    end
-    it "http DELETE should not be available" do
-      expect(:delete => "/annotations/search").not_to be_routable
-      expect(:delete => "/search").not_to be_routable
-      expect(:delete => "/annotations/search/666").not_to be_routable
-    end
-    it "http PUT should not be available" do
-      expect(:put => "/annotations/search").not_to be_routable
-      expect(:put => "/search").not_to be_routable
-      expect(:put => "/annotations/search/666").not_to be_routable
-    end
-    it "http PATCH should not be available" do
-      expect(:patch => "/annotations/search").not_to be_routable
-      expect(:patch => "/search").not_to be_routable
-      expect(:patch => "/annotations/search/666").not_to be_routable
-    end
-  end
-
-=end
+  end # search
 
 end
