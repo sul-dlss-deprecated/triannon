2362016928503f63544f604dc5d48362c328bebc
Darren L. Weber, Ph.D <darren.weber@stanford.edu>
2015-06-22 12:56:55 -0700

    Revise routing exceptions to handle /auth paths

--- config/routes.rb
+++ config/routes.rb
@@ -16,76 +16,58 @@ Triannon::Engine.routes.draw do
   # get -> new action
   get '/annotations/:anno_root/new', to: 'annotations#new'
   get '/:anno_root/new', to: 'annotations#new',
-    constraints: lambda { |request|
-      anno_root = request.path_parameters[:anno_root]
-      id = request.path_parameters[:id]
-      anno_root !~ /^annotations$/ && anno_root !~ /^search$/ && anno_root !~ /^new$/ && id !~ /^search$/ && id !~ /^new$/
-    }
+    constraints: lambda { |request| anno_container_exclusions request }
 
   # get -> search controller find action
   get '/annotations/:anno_root/search', to: 'search#find'
   get '/annotations/search', to: 'search#find'
   get '/:anno_root/search', to: 'search#find',
-    constraints: lambda { |request|
-      anno_root = request.path_parameters[:anno_root]
-      anno_root !~ /^annotations$/ && anno_root !~ /^search$/ && anno_root !~ /^new$/
-    }
+    constraints: lambda { |request| anno_container_exclusions request }
   get '/search', to: 'search#find'
 
   # get w id -> show action
   get '/annotations/:anno_root/:id(.:format)', to: 'annotations#show',
-    constraints: lambda { |request|
-      anno_root = request.path_parameters[:anno_root]
-      anno_root !~ /^search$/ && anno_root !~ /^new$/
-    }
+    constraints: lambda { |request| annotations_container_exclusions request }
   get '/:anno_root/:id(.:format)', to: 'annotations#show',
-    constraints: lambda { |request|
-      anno_root = request.path_parameters[:anno_root]
-      id = request.path_parameters[:id]
-      anno_root !~ /^annotations$/ && anno_root !~ /^search$/ && anno_root !~ /^new$/ && id !~ /^search$/ && id !~ /^new$/
-    }
+    constraints: lambda { |request| anno_container_exclusions request }
 
   # get no id -> index action
   get '/annotations/:anno_root', to: 'annotations#index',
-    constraints: lambda { |request|
-      anno_root = request.path_parameters[:anno_root]
-      anno_root !~ /^search$/ && anno_root !~ /^new$/
-    }
+    constraints: lambda { |request| annotations_container_exclusions request }
   get '/:anno_root', to: 'annotations#index',
-    constraints: lambda { |request|
-      anno_root = request.path_parameters[:anno_root]
-      anno_root !~ /^annotations$/ && anno_root !~ /^search$/ && anno_root !~ /^new$/
-    }
+    constraints: lambda { |request| anno_container_exclusions request }
 
   # post -> create action
   post '/annotations/:anno_root', to: 'annotations#create',
-    constraints: lambda { |request|
-      anno_root = request.path_parameters[:anno_root]
-      id = request.path_parameters[:id]
-      anno_root !~ /^search$/ && anno_root !~ /^new$/ && id !~ /^search$/ && id !~ /^new$/
-    }
+    constraints: lambda { |request| annotations_container_exclusions request }
   post '/:anno_root', to: 'annotations#create',
-    constraints: lambda { |request|
-      anno_root = request.path_parameters[:anno_root]
-      anno_root !~ /^annotations$/ && anno_root !~ /^search$/ && anno_root !~ /^new$/
-    }
+    constraints: lambda { |request| anno_container_exclusions request }
 
   # delete -> destroy action
   delete '/annotations/:anno_root/:id(.:format)', to: 'annotations#destroy',
-    constraints: lambda { |request|
-      anno_root = request.path_parameters[:anno_root]
-      id = request.path_parameters[:id]
-      anno_root !~ /^search$/ && anno_root !~ /^new$/ && id !~ /^search$/ && id !~ /^new$/
-    }
+    constraints: lambda { |request| annotations_container_exclusions request }
   delete '/:anno_root/:id(.:format)', to: 'annotations#destroy',
-    constraints: lambda { |request|
-      anno_root = request.path_parameters[:anno_root]
-      anno_root !~ /^annotations$/ && anno_root !~ /^search$/ && anno_root !~ /^new$/
-    }
-
+    constraints: lambda { |request| anno_container_exclusions request }
 
   get '/annotations', to: 'search#find'
   root to: 'search#find'
 
-
 end
+
+
+def anno_container_exclusions(request)
+  anno_root = request.path_parameters[:anno_root]
+  anno_root !~ /^annotations|auth|new|search$/ && id_exclusions(request)
+end
+
+def annotations_container_exclusions(request)
+  anno_root = request.path_parameters[:anno_root]
+  anno_root !~ /^auth|new|search$/ && id_exclusions(request)
+end
+
+def id_exclusions(request)
+  return true if request.path_parameters[:id].nil?
+  request.path_parameters[:id] !~ /^new|search$/
+end
+
+
